c7c470e0bca33ac9e70884b34165b03252f9e1e0
grunam <tiabnamik@gmail.com>
2018-11-01 12:14:58 +0100

    création du TAG

---
+++ script/filler.js
@@ -0,0 +1,80 @@
+
+var socket = io.connect('http://localhost:9999');
+var nickname  = prompt('Quel est votre pseudo ?');
+while(nickname==='' || nickname === null){
+    nickname  = prompt('Quel est votre pseudo ?'); 
+}
+socket.emit('login', {pseudo: nickname});
+document.title = nickname + ' - ' + document.title; 
+  
+socket.on('add', function(data){
+    addMessage(data, 'added');
+    addNewtodo(data.task);
+});
+
+socket.on('remove', function(data){
+    addMessage(data, 'removed');
+    $("#todolist li").eq(data.task).remove();
+});
+
+socket.on('messageLog', function(data){  
+    addMessageLog(data);
+});
+
+socket.on('loadTodolist', function(todolist){
+     $('#todolist').empty();
+     todolist.forEach(function(item, index) {
+        addNewtodo(item);
+     });           
+});
+
+
+socket.on('connectedUsersList', function(connectedUsers){
+     $('#usersList').empty();
+     connectedUsers.forEach(function(item, index) {
+        addUser(item);
+     });           
+});
+
+
+$("#todolist").on("click", "a", function() {
+    var id = $(this).closest('li').index();
+    socket.emit('remove', id);
+    $(this).closest('li').remove();    
+    return false;
+}); 
+
+ $('#formulaire_todolist').submit(function(){
+   var newtodo = $('#newtodo').val();
+   socket.emit('add', newtodo);
+   addNewtodo(newtodo);
+   $('#newtodo').val('').focus();
+   return false;
+});
+
+function addMessage(data, action){
+    $('#message li').empty();
+    if(isNaN(data.task)){
+        $('#message li').append(data.pseudo +' has '+ action +' one task: "'+ data.task + '"');
+    }else{
+        var task = $("#todolist li").eq(data.task).html();
+        var result = task.split("</a>");
+        //var result = task.replace(/<a [^>]+>[^<]*<\/a>/, '');
+        $('#message li').append(data.pseudo +' a '+ action +' one task: "'+  result[1] +'"');
+        //$('#message').append(data.pseudo +' a '+action+' la tâche: '+  result);
+    }
+}
+
+function addMessageLog(data){
+    $('#message li').empty();
+    $('#message li').append(data.pseudo +' has '+ data.message);
+}
+
+function addNewtodo(newtodo){
+    $('#todolist').append('<li class="list-group-item"><a href="#_">✘</a>'+ newtodo +'</li>');
+}
+
+function addUser(user){
+    $('#usersList').append('<li class="text-danger">'+ user +'</li>');
+}  
+
---
+++ server.js
@@ -0,0 +1,73 @@
+
+var todolist = []; 
+var connectedUsers = [];
+
+var express = require('express'),
+ app = express(),  
+ server = require('http').createServer(app),
+ io = require('socket.io')(server),
+ ent = require('ent'),
+
+session = require("express-session")({
+   secret: "my-secret",
+   resave: true,
+   saveUninitialized: true
+}),
+sharedsession = require("express-socket.io-session");
+ 
+ 
+ 
+app.use(express.static('./'))
+.use(session)
+.get('/', function(req, res){
+     res.sendFile(__dirname + '/index.html');
+ })
+.use(function(req, res, next){
+   res.redirect('/');         
+});
+ 
+io.use(sharedsession(session, {
+    autoSave:true
+})); 
+
+io.on('connection', function(socket){
+    
+    socket.emit('loadTodolist', todolist);
+
+     socket.on("login", function(data) {
+        pseudo = ent.encode(data.pseudo);
+        socket.handshake.session.userdata = pseudo;
+        socket.handshake.session.save();
+        socket.broadcast.emit('messageLog', {pseudo: socket.handshake.session.userdata, message: '<u>logged in !</u>'});
+        connectedUsers.push(pseudo);
+        io.sockets.emit('connectedUsersList', connectedUsers);
+        
+    });
+    
+    
+     socket.on('add', function(newtodo){
+        newtodo = ent.encode(newtodo);
+        todolist.push(newtodo);
+        socket.broadcast.emit('add', {pseudo: socket.handshake.session.userdata, task: newtodo});
+     })
+     
+     socket.on('remove', function(id){
+         todolist.splice(id, 1);
+         socket.broadcast.emit('remove', {pseudo: socket.handshake.session.userdata, task: id});
+     })
+     
+     socket.on('disconnect', function() {
+        if (socket.handshake.session.userdata) {
+            socket.broadcast.emit('messageLog', {pseudo: socket.handshake.session.userdata, message: '<u>logged out !</u>'});
+            connectedUsers.splice(connectedUsers.indexOf(socket.handshake.session.userdata), 1);
+            socket.broadcast.emit('connectedUsersList', connectedUsers);
+            delete socket.handshake.session.userdata;
+            socket.handshake.session.save();
+        }
+    });  
+ 
+}); 
+
+ server.listen(9999);
+
+
