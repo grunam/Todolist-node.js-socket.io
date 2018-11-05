455bcde7809899ca2f51dfa540d0550e71f0d13a
grunam <tiabnamik@gmail.com>
2018-11-05 10:15:30 +0100

    - Amélioration de la présentation des tâches de la todolist avec bootstrap - implémentation de la persistance des données avec MongoDB Atlas et mangoose - modification de la liste des tâche par le client (ajout et suppression) via le server et socket.io et non plus en javascript côté client (avec toujours la modification de la liste des autres utilisateurs via le le server et socket.io)

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
+//TODO détecter fin du socket ou de la session (arrêt processus serveur)
+
 
 var todolist = []; 
 var connectedUsers = [];
 
 
-var MongoClient = require('mongodb').MongoClient;
-var uri = "mongodb+srv://tiabnamik:lI20obpPKFbFSWyy@cluster0-bj2hr.mongodb.net/todolist?retryWrites=true";
-MongoClient.connect(uri,  { useNewUrlParser: true }, function(err, client) {
-    if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-   }
-   console.log('Connected to MongoDB Atlas database...');
-    var collection = client.db("todolist").collection("tasks");
-   // perform actions on the collection object
+var mongoose = require('mongoose');
+var dateFormat = require('dateformat');
 
+var uri = "mongodb+srv://tiabnamik:lI20obpPKFbFSWyy@cluster0-bj2hr.mongodb.net/todolist?retryWrites=true";
+
+
+mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+var taskSchema = new mongoose.Schema({
+  name : String,
+  user: String,
+  date : Date 
+});
+
+
+var taskModel = mongoose.model('task', taskSchema);
+taskModel.find(null, function (err, result) {
+  if (err) { 
+      //throw err;
+      console.log('Error occurred while finding all task from MongoDB Atlas...\n',err);
+  }
+ 
+  //console.log(result);
+  if(result.length !== 0){
+    (function(a){
+        for (var i = 0, len = a.length; i < len; i++){
+            todolist.push({id:a[i]['_id'],  name: a[i]['name'], user: a[i]['user'], date: dateFormat(a[i]['date'], "dddd, mmmm dS, yyyy, h:MM:ss TT") });   
+        }
+    })(result);
+  }
+  console.log('ma todolist: '+todolist);
+  mongoose.connection.close();
 });
 
 var express = require('express'),
@@ -66,35 +96,68 @@ io.use(sharedsession(usedSession, {
  
 io.on('connection', function(socket){
     
+    console.log(todolist);
     socket.emit('loadTodolist', todolist);
 
      socket.on("login", function(data) {
-        pseudo = ent.encode(data.pseudo);
-        socket.handshake.session.userdata = pseudo;
+        user = ent.encode(data.user);
+        socket.handshake.session.userdata = user;
         socket.handshake.session.save();
-        socket.broadcast.emit('messageLog', {pseudo: socket.handshake.session.userdata, message: '<u>logged in !</u>'});
-        connectedUsers.push(pseudo);
+        io.sockets.emit('messageLog', {user: socket.handshake.session.userdata, message: '<u>logged in !</u>'});
+        connectedUsers.push(user);
         io.sockets.emit('connectedUsersList', connectedUsers);
     });
    
      socket.on('add', function(newtodo){
         newtodo = ent.encode(newtodo);
-        todolist.push(newtodo);
-        socket.broadcast.emit('add', {pseudo: socket.handshake.session.userdata, task: newtodo});
+        var now = Date.now();
+      
+        mongoose.connect(uri, { useNewUrlParser: true });
+        //mongoose.Promise = global.Promise;
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+        var myTask = new taskModel({name : newtodo, user: socket.handshake.session.userdata, date: now});
+       
+        myTask.save(function (err) {
+            if (err) { 
+               //throw err;
+                console.log('Error occurred while saving task from MongoDB Atlas...\n',err);
+            }
+            console.log('task has been successfuly added !');
+            // On se déconnecte de MongoDB maintenant
+            mongoose.connection.close();
+        });
+        todolist.push({name : newtodo, user: socket.handshake.session.userdata, date: dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT") });
+        console.log(todolist);
+        io.sockets.emit('add', {name : newtodo, user: socket.handshake.session.userdata, date: dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT") });
      })
      
      socket.on('remove', function(id){
+        mongoose.connect(uri, { useNewUrlParser: true });
+        //mongoose.Promise = global.Promise;
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+        //var myTask = new taskModel({name : newtodo, user: socket.handshake.session.userdata, date: Date.now()});
+       taskModel.findByIdAndDelete({_id:todolist[id].id}, function (err, result) {
+          if (err) { 
+              //throw err;
+              console.log('Error occurred while removing task from MongoDB Atlas...\n',err);
+          }
+          console.log('task has been successfuly removed !');
+          mongoose.connection.close();
+        });
          todolist.splice(id, 1);
-         socket.broadcast.emit('remove', {pseudo: socket.handshake.session.userdata, task: id});
+         console.log(todolist);
+         io.sockets.emit('remove', {user: socket.handshake.session.userdata, task: id});
      })
      
      socket.on('disconnect', function() {
         if (socket.handshake.session.userdata) {
-            socket.broadcast.emit('messageLog', {pseudo: socket.handshake.session.userdata, message: '<u>logged out !</u>'});
+            socket.broadcast.emit('messageLog', {user: socket.handshake.session.userdata, message: '<u>logged out !</u>'});
             connectedUsers.splice(connectedUsers.indexOf(socket.handshake.session.userdata), 1);
+            //socket.handshake.session.save();
             socket.broadcast.emit('connectedUsersList', connectedUsers);
             delete socket.handshake.session.userdata;
-            socket.handshake.session.save();
         }
     });  
  
